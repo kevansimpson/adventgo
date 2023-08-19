59fd402a6f7cf0ef97b1e3d216a77f626110e274
Kevan Simpson Air1 <kevan.simpson@gmail.com>
2023-08-19 10:27:42 -0700

    move input files to subfolder

--- pkg/aoc2015/aoc2015_test.go
+++ pkg/aoc2015/aoc2015_test.go
@@ -8,20 +8,20 @@ import (
 )
 
 func TestDay01Solutions(t *testing.T) {
-	input := util.ReadSingleLine("input01.txt")
+	input := util.ReadSingleLine("data/input01.txt")
 	assert.Equal(t, 74, whatFloorSanta(input), "whatFloorSanta")
 	assert.Equal(t, 1795, santaEntersBasement(input), "santaEntersBasement")
 }
 
 func TestDay02Solutions(t *testing.T) {
-	input := util.ReadLines("input02.txt")
+	input := util.ReadLines("data/input02.txt")
 	paper, ribbon := howMuchPaperAndRibbon(input)
 	assert.Equal(t, 1588178, paper, "howMuchWrappingPaper")
 	assert.Equal(t, 3783758, ribbon, "howMuchRibbon")
 }
 
 func TestDay03Solutions(t *testing.T) {
-	input := util.ReadSingleLine("input03.txt")
+	input := util.ReadSingleLine("data/input03.txt")
 	assert.Equal(t, 2081, santaRoute(input), "santaRoute")
 	assert.Equal(t, 2341, roboSantaRoute(input), "roboSantaRoute")
 }
@@ -32,28 +32,35 @@ func TestDay04Solutions(t *testing.T) {
 }
 
 func TestDay05Solutions(t *testing.T) {
-	input := util.ReadLines("input05.txt")
+	input := util.ReadLines("data/input05.txt")
 	assert.Equal(t, 258, oldNiceStrings(input), "oldNiceStrings")
 	assert.Equal(t, 53, newNiceStrings(input), "newNiceStrings")
 }
 
 func TestDay06Solutions(t *testing.T) {
-	input := util.ReadLines("input06.txt")
+	input := util.ReadLines("data/input06.txt")
 	lightsLit, totalBrightness := followLightCommands(input)
 	assert.Equal(t, 543903, lightsLit, "lightsLit")
 	assert.Equal(t, 14687245, totalBrightness, "totalBrightness")
 }
 
 func TestDay07Solutions(t *testing.T) {
-	input := util.ReadLines("input07.txt")
+	input := util.ReadLines("data/input07.txt")
 	signalWireA, overrideSignal := assembleCircuits(input)
 	assert.Equal(t, 46065, signalWireA, "signalWireA")
 	assert.Equal(t, 14134, overrideSignal, "overrideSignal")
 }
 
 func TestDay08Solutions(t *testing.T) {
-	input := util.ReadLines("input08.txt")
+	input := util.ReadLines("data/input08.txt")
 	oldEncoding, newEncoding := encodeSantasList(input)
 	assert.Equal(t, 1333, oldEncoding, "oldEncoding")
 	assert.Equal(t, 2046, newEncoding, "newEncoding")
 }
+
+// func TestDay09Solutions(t *testing.T) {
+// 	input := util.ReadLines("data/input09.txt")
+// 	shortestRoute, longestRoute := calculateSantaRoutes(input)
+// 	assert.Equal(t, 207, shortestRoute, "shortestRoute")
+// 	assert.Equal(t, 804, longestRoute, "longestRoute")
+// }
