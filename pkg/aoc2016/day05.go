e3873056abbb4b96fafc76540e22cef4c6839b03
Kevan Simpson Air1 <kevan.simpson@gmail.com>
2023-08-30 10:09:08 -0700

    final solution day05, 2016

--- pkg/aoc2016/day05.go
+++ pkg/aoc2016/day05.go
@@ -5,7 +5,6 @@ package aoc2016
  */
 
 import (
-	"fmt"
 	"strings"
 	"sync"
 
@@ -16,47 +15,28 @@ type Day05 struct{}
 
 func (d Day05) decodeDoorPassword(input string) (string, string) {
 	var wg sync.WaitGroup
-	channelHash := make(chan string, 17)
-	channelDone := make(chan bool)
+	channelHash := make(chan string, 12)
+	channelPswd := make(chan string)
 	wg.Add(1)
 
-	go d.nextHashPassword(input, channelHash, channelDone, &wg)
+	go d.nextHashPassword(input, channelHash, channelPswd, &wg)
 
-	firstPswd, secondPswd := make([]rune, 8), []rune{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
-	count := 0
+	firstPswd := make([]rune, 8)
 	for i := 0; i < 8; i++ {
 		next := <-channelHash
 		firstPswd[i] = rune(next[5])
-
-		if strings.Contains("01234567", string(next[5])) {
-			digit := int(next[5] - '0')
-			if digit < 8 && secondPswd[digit] == ' ' {
-				secondPswd[digit] = rune(next[6])
-				count++
-			}
-		}
 	}
 
-	for count < 8 {
-		next := <-channelHash
-		if strings.Contains("01234567", string(next[5])) {
-			digit := int(next[5] - '0')
-			if digit < 8 && secondPswd[digit] == ' ' {
-				secondPswd[digit] = rune(next[6])
-				count++
-			}
-		}
-	}
+	pswd2 := <-channelPswd
 
-	channelDone <- true
 	wg.Wait()
 	close(channelHash)
-	close(channelDone)
+	close(channelPswd)
 
-	return string(firstPswd), string(secondPswd)
+	return string(firstPswd), pswd2
 }
 
-func (d Day05) nextHashPassword(input string, chHash chan<- string, chDone <-chan bool, wg *sync.WaitGroup) {
+func (d Day05) nextHashPassword(input string, chHash chan<- string, chPswd chan<- string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	// start is determined in hindsight from final solution.
 	// calculating md5 hashes is slow, full stop.
@@ -67,24 +47,28 @@ func (d Day05) nextHashPassword(input string, chHash chan<- string, chDone <-cha
 		4515059, 6924074, 8038154, 13432968, 13540621, 14095580, 14821988, 16734551,
 		// next 5
 		17743256, 19112977, 20616595, 21658552, 26326685}
-	start, count := known[0], 0
-	for true {
+	start, count, p2 := known[0], 0, 0
+	secondPswd := []rune{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
 
-		select {
-		case doneNow := <-chDone:
-			if doneNow {
-				return
-			}
-		default:
-			next, ix := util.NextHash(input, "00000", start)
-			chHash <- next
-			count++
-			if util.IsFullSolve() || count > 12 {
-				start = ix + 1
-			} else {
-				start = known[count]
-				fmt.Println(start)
+	for p2 < 8 {
+		next, ix := util.NextHash(input, "00000", start)
+		chHash <- next
+
+		if strings.Contains("01234567", string(next[5])) {
+			digit := int(next[5] - '0')
+			if digit < 8 && secondPswd[digit] == ' ' {
+				secondPswd[digit] = rune(next[6])
+				p2++
 			}
 		}
+
+		count++
+		if util.IsFullSolve() || count > 12 {
+			start = ix + 1
+		} else {
+			start = known[count]
+		}
 	}
+
+	chPswd <- string(secondPswd)
 }
